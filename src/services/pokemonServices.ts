fa6b8682b87c1d48b6ecc11b6cc067fc7a9cf38e
Laura <laura.tracierra@gmail.com>
2021-08-05 21:03:44 -0300

    nova tentativa de resposta correta

--- src/services/pokemonServices.ts
+++ src/services/pokemonServices.ts
@@ -2,22 +2,29 @@ import { getRepository } from "typeorm";
 import Pokemons from "../entities/Pokemons";
 import UserPokemons from "../entities/userPokemons";
 
+interface AddUser extends Pokemons{
+    inMyPokemons: boolean
+}
+
 export async function listPokemons(id: number){
     const userPokemons = await getRepository(UserPokemons).find({
         where:{
             userId: id
         }
     });
-    let pokemons: any = await getRepository(Pokemons).find();
+    let pokemons: any[] = await getRepository(Pokemons).find();
+    if(!userPokemons){
+        return pokemons.map(n => n["inMyPokemons"] = false)
+    };
 
     for(let i = 0; i < pokemons.length; i++){
         const pokemonId = pokemons[i].id;
         for(let j = 0; j < userPokemons.length; j++){
-            if(userPokemons[j].id === pokemonId){
-                pokemons[i].inMyPokemons = true;
+            if(userPokemons[j].pokemonId === pokemonId){
+                pokemons[i]["inMyPokemons"] = true;
             }
             else{
-                pokemons[i].inMyPokemons = false;
+                pokemons[i]["inMyPokemons"] = false;
             }
         }
     }
