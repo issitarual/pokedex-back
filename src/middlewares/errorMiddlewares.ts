67ae47207e5580c10880f7a0d80d8fd8de0b2032
thiagoribb <thiribeiro142@gmail.com>
2021-08-16 16:11:40 -0300

    Preprando review

--- src/middlewares/errorMiddlewares.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export function errorMiddleware(err: Error, req: Request, res: Response, next: NextFunction){
-    console.error(err.stack);
-    res.sendStatus(500);
-}
